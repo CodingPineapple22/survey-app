6b746169e1a7d53e7af596c98f4eec1191a39b57
CodingPineapple22 <leothomasthefirst@gmail.com>
2023-04-03 21:13:41 -0400

    edited the json.js file to ask a user for string questions

--- src/json.js
+++ src/json.js
@@ -1,34 +1,25 @@
 export const json = {
   "elements": [{
-    "name": "number",
+    "name": "text",
     "type": "text",
-    "title": "Enter a number from 0 to 100",
-    "inputType": "number",
-    "min": 0,
-    "max": 100,
-    "defaultValue": 0,
+    "title": "What is your first question? ",
+    "inputType": "text",
+    "defaultValue": "",
     "isRequired": true
   }, {
-    "name": "range",
+    "name": "text",
     "type": "text",
-    "title": "Select a value",
-    "inputType": "range",
-    "min": 0,
-    "max": 100,
-    "step": 10,
-    "defaultValue": 90
+    "title": "What is your second question? ",
+    "inputType": "text",
+    "defaultValue": "",
+    "isRequired": true
   }, {
-    "name": "phone",
+    "name": "text",
     "type": "text",
-    "title": "Enter a phone number",
-    "inputType": "tel",
-    "placeholder": "+0 (000) 000-00-00",
-    "autocomplete": "tel",
-    "validators": [{
-      "type": "regex",
-      "regex": "\\+[0-9]{1} \\([0-9]{3}\\) [0-9]{3}-[0-9]{2}-[0-9]{2}",
-      "text": "Phone number must be in the following format: +0 (000) 000-00-00"
-    }]
+    "title": "What is your third question? ",
+    "inputType": "text",
+    "defaultValue": "",
+    "isRequired": true
   }],
   "showQuestionNumbers": false
 };
